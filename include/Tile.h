4e257d55292bf2cd82747ba9d169b06d4b09b87b
drunicornthe1 <drunicornthe1@users.noreply.github.com>
2017-01-19 16:48:55 -0600

    Update Tile.h
    We did stuff.

--- include/Tile.h
+++ include/Tile.h
@@ -1,4 +1,25 @@
 #ifndef __TILE__
 #define __TILE__
 
+#include <vector>
+
+using namespace std;
+
+class Tile
+{
+public:
+
+  vector <Tile*> getConnections();
+  //Need to figure out geometry of Tile 
+private:
+
+  Tile();
+  
+  float x, y, z;
+  int w = 0;
+  
+  list <Tile*> connections;
+  
+};
+
 #endif
